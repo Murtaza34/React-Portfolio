7d1c5944cde8731aaa40ba6228ac7540b9cbff69
unknown <you@example.com>
2023-03-18 21:24:06 +0000

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import Footer from "./components/Footer";
 
-function App() {
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Jost", "sans-serif"].join(","),
+    fontWeightLight: "300",
+    fontWeightRegular: "400",
+    fontWeightBold: "500",
+  },
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <Navbar />;
+        {/* <Footer />; */}
+      </ThemeProvider>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,112 @@
+import React from "react";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Button,
+  Container,
+  Typography,
+} from "@mui/material/";
+import MenuIcon from "@mui/icons-material/Menu";
+import logo from "../Logo.svg";
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "projects", "Contact", "Resume"];
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    // SideNavBar reveal on mobile
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Box>
+        <img src={logo} alt="logo" height="80" width="60" />
+      </Box>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{ bgcolor: "#f8f9fa" }}>
+        <Container>
+          <Toolbar>
+            <IconButton
+              color="#353535"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            {/* Personal logo */}
+            <Box sx={{ flexGrow: 1, textAlign: { xs: "center", sm: "left" } }}>
+              <img src={logo} alt="logo" height="80" width="60" />
+            </Box>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item}
+                  sx={{
+                    color: "#353535",
+                    fontSize: "1rem",
+                    fontFamily: "Jost",
+                    fontWeight: "400",
+                    textTransform: "none",
+                  }}
+                >
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
+}
+
+export default DrawerAppBar;
